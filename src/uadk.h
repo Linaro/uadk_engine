2c4fc0f2ca7e6a13b8a1e6bb42d88645d06751a0
Zhangfei Gao <zhangfei.gao@linaro.org>
2020-09-24 02:41:47 +0000

    Add rsa engine function
    Cipher Test:
    openssl engine -c uadk
    openssl enc -aes-128-cbc -engine uadk -pass pass:123456
    openssl enc -aes-192-cbc -engine uadk -pass pass:123456
    openssl enc -aes-256-cbc -engine uadk -pass pass:123456
    openssl enc -aes-128-ctr -engine uadk -pass pass:123456
    openssl enc -aes-192-ctr -engine uadk -pass pass:123456
    openssl enc -aes-256-ctr -engine uadk -pass pass:123456
    
    md5 test:
    openssl speed -engine uadk hmac
    
    Fix:
    1. uadk will install head files in /usr/local/include/uadk, so let use correct
    head file, otherwise data will be wrong.
    2. replace ctx to sess.
    3. rename uadk_ras_sess.
    4. fix ctx_mode bug.
    5. introduce error print function.
    6. add callback functions for genkey, enc, dec, sign, verify.
    7. delete uadk_rsa_crypto(), use uadk_rsa_sync() instead.
    8. clean uadk_rsa_fill_prikey(),add uadk_rsa_fill_prikey_crt().
    9. rsa: remove init/destroy to e_uadk.c.
    10. fix resource init & uninit.
    11. remove no need macro.
    12. make functions to static.
    13. fix ENGINE_set_RSA() input.
    14. add some missing functions.
    15. add pubkey check function.
    
    RSA functions:
    So far, RSA-keygen,RSA-encrypt,RSA-decrypt,RSA-sign,RSA-verify SYNC MODE functions are OK.
    The test commands:
    1. Generate RSA private key
    openssl genrsa -out prikey.pem -engine uadk 2048
    2. Genetrate RSA public key
    openssl rsa -in prikey.pem -pubout -out pubkey.pem -engine uadk
    3. RSA encrypt
    openssl rsautl -encrypt -in plain.txt -inkey pubkey.pem -pubin -out enc.txt -engine uadk
    4. RSA decrypt
    openssl rsautl -decrypt -in enc.txt -inkey prikey.pem -out dec.txt -engine uadk
    5. RSA sign
    openssl rsautl -sign -in msg.txt -inkey prikey.pem -out signed.txt -engine uadk
    6. RSA verify
    openssl rsautl -verify -in signed.txt -inkey pubkey.pem -pubin -out verified.txt -engine uadk
    7. Speed
    openssl speed -elapsed -engine uadk rsa2048
    
    -RSA engine--- Edit by: Zhou Wang, Zhiqi Song.
    
    Signed-off-by: Zhiqi Song <songzhiqi1@huawei.com>
    
    fix coding style
    
    Signed-off-by: Zhiqi Song <songzhiqi1@huawei.com>

---
+++ src/uadk.h
@@ -0,0 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+extern int uadk_bind_cipher(ENGINE *e);
+extern void uadk_destroy_cipher(void);
+extern int uadk_bind_digest(ENGINE *e);
+extern void uadk_destroy_digest(void);
+extern void uadk_destroy_digest(void);
+
+extern RSA_METHOD *uadk_get_rsa_methods(void);
+extern int uadk_init_rsa(void);
+extern void uadk_destroy_rsa(void);
+
+#define uadk_eng_err(format, args...)			\
+	fprintf(stderr, "ue: %s: "format, __func__, ##args)
+
