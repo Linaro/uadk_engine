a913dbd026eca81e488d39261552d681b73da2ca
Zhangfei Gao <zhangfei.gao@linaro.org>
2021-01-04 09:16:00 +0000

    ras: work with nginx via both async and sync mode
    /usr/local/ssl/openssl.cnf
    default_algorithms = RSA, DSA, RAND
    
    async:
    conf/nginx.conf
    http {
        ssl_async  on;
    }
    
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>

--- src/uadk.h
+++ src/uadk.h
@@ -23,5 +23,5 @@ extern void uadk_destroy_cipher(void);
 extern int uadk_bind_digest(ENGINE *e, struct uacce_dev_list *list);
 extern void uadk_destroy_digest(void);
 extern void uadk_destroy_rsa(void);
-extern int uadk_bind_rsa(ENGINE *e, struct uacce_dev_list *list);
+extern int uadk_bind_rsa(ENGINE *e);
 #endif
