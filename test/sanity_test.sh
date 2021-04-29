2a974622bd15e59cc90511d10f06241dccd54747
Zhangfei Gao <zhangfei.gao@linaro.org>
2021-04-29 04:15:33 +0000

    uadk: add sanity test
    Add sanity test for openssl-uadk
    Submitting new patch requires passing sanity_test.sh
    
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>

---
+++ test/sanity_test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+chmod 666 /dev/hisi_*
+
+openssl engine -c uadk
+
+#digest
+openssl speed -engine uadk -evp md5
+openssl speed -engine uadk -async_jobs 1 -evp md5
+openssl speed -engine uadk -evp sm3
+openssl speed -engine uadk -async_jobs 1 -evp sm3
+openssl speed -engine uadk -evp sha1
+openssl speed -engine uadk -async_jobs 1 -evp sha1
+openssl speed -engine uadk -evp sha256
+openssl speed -engine uadk -async_jobs 1 -evp sha256
+openssl speed -engine uadk -evp sha512
+openssl speed -engine uadk -async_jobs 1 -evp sha512
+
+#cipher
+openssl speed -engine uadk -evp aes-128-cbc
+openssl speed -engine uadk -async_jobs 1 -evp aes-128-cbc
+openssl speed -engine uadk -evp aes-192-cbc
+openssl speed -engine uadk -async_jobs 1 -evp aes-192-cbc
+openssl speed -engine uadk -evp aes-256-cbc
+openssl speed -engine uadk -async_jobs 1 -evp aes-256-cbc
+openssl speed -engine uadk -evp aes-128-ecb
+openssl speed -engine uadk -async_jobs 1 -evp aes-128-ecb
+openssl speed -engine uadk -evp aes-192-ecb
+openssl speed -engine uadk -async_jobs 1 -evp aes-192-ecb
+openssl speed -engine uadk -evp aes-256-ecb
+openssl speed -engine uadk -async_jobs 1 -evp aes-256-ecb
+openssl speed -engine uadk -evp aes-128-xts
+openssl speed -engine uadk -async_jobs 1 -evp aes-128-xts
+openssl speed -engine uadk -evp aes-256-xts
+openssl speed -engine uadk -async_jobs 1 -evp aes-256-xts
+
+#rsa
+openssl speed -elapsed -engine uadk rsa1024
+openssl speed -elapsed -engine uadk -async_jobs 1 rsa1024
+openssl speed -elapsed -engine uadk rsa2048
+openssl speed -elapsed -engine uadk -async_jobs 1 rsa2048
+openssl speed -elapsed -engine uadk rsa4096
+openssl speed -elapsed -engine uadk -async_jobs 1 rsa4096
