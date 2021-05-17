f223cf07636b8a8efa07b1586fc51f71ca37a1df
Zhangfei Gao <zhangfei.gao@linaro.org>
2021-05-17 06:19:35 +0000

    sanity_test: test only if the algorithm exists
    Different platforms may have different accelerators,
    Test only if the algorithm exists
    
    Signed-off-by: Zhangfei Gao <zhangfei.gao@linaro.org>

--- test/sanity_test.sh
+++ test/sanity_test.sh
@@ -2,50 +2,77 @@
 
 chmod 666 /dev/hisi_*
 
-openssl engine -c uadk
+algs=$(openssl engine -c uadk)
+echo $algs
 
 #digest
-openssl speed -engine uadk -evp md5
-openssl speed -engine uadk -async_jobs 1 -evp md5
-openssl speed -engine uadk -evp sm3
-openssl speed -engine uadk -async_jobs 1 -evp sm3
-openssl speed -engine uadk -evp sha1
-openssl speed -engine uadk -async_jobs 1 -evp sha1
-openssl speed -engine uadk -evp sha256
-openssl speed -engine uadk -async_jobs 1 -evp sha256
-openssl speed -engine uadk -evp sha512
-openssl speed -engine uadk -async_jobs 1 -evp sha512
+if [[ $algs =~ "MD5" ]]; then
+	echo "testing MD5"
+	openssl speed -engine uadk -evp md5
+	openssl speed -engine uadk -async_jobs 1 -evp md5
+fi
+
+if [[ $algs =~ "SM3" ]]; then
+	echo "testing SM3"
+	openssl speed -engine uadk -evp sm3
+	openssl speed -engine uadk -async_jobs 1 -evp sm3
+fi
+
+if [[ $algs =~ "SHA" ]]; then
+	echo "testing SHA"
+	openssl speed -engine uadk -evp sha1
+	openssl speed -engine uadk -async_jobs 1 -evp sha1
+	openssl speed -engine uadk -evp sha256
+	openssl speed -engine uadk -async_jobs 1 -evp sha256
+	openssl speed -engine uadk -evp sha512
+	openssl speed -engine uadk -async_jobs 1 -evp sha512
+fi
 
 #cipher
-openssl speed -engine uadk -evp aes-128-cbc
-openssl speed -engine uadk -async_jobs 1 -evp aes-128-cbc
-openssl speed -engine uadk -evp aes-192-cbc
-openssl speed -engine uadk -async_jobs 1 -evp aes-192-cbc
-openssl speed -engine uadk -evp aes-256-cbc
-openssl speed -engine uadk -async_jobs 1 -evp aes-256-cbc
-openssl speed -engine uadk -evp aes-128-ecb
-openssl speed -engine uadk -async_jobs 1 -evp aes-128-ecb
-openssl speed -engine uadk -evp aes-192-ecb
-openssl speed -engine uadk -async_jobs 1 -evp aes-192-ecb
-openssl speed -engine uadk -evp aes-256-ecb
-openssl speed -engine uadk -async_jobs 1 -evp aes-256-ecb
-openssl speed -engine uadk -evp aes-128-xts
-openssl speed -engine uadk -async_jobs 1 -evp aes-128-xts
-openssl speed -engine uadk -evp aes-256-xts
-openssl speed -engine uadk -async_jobs 1 -evp aes-256-xts
-openssl speed -engine uadk -evp sm4-cbc
-openssl speed -engine uadk -async_jobs 1 -evp sm4-cbc
-openssl speed -engine uadk -evp sm4-ecb
-openssl speed -engine uadk -async_jobs 1 -evp sm4-ecb
-openssl speed -engine uadk -evp des-ede3-cbc
-openssl speed -engine uadk -async_jobs 1 -evp des-ede3-cbc
-openssl speed -engine uadk -evp des-ede3-ecb
-openssl speed -engine uadk -async_jobs 1 -evp des-ede3-ecb
+
+if [[ $algs =~ "AES" ]]; then
+	echo "testing AES"
+	openssl speed -engine uadk -evp aes-128-cbc
+	openssl speed -engine uadk -async_jobs 1 -evp aes-128-cbc
+	openssl speed -engine uadk -evp aes-192-cbc
+	openssl speed -engine uadk -async_jobs 1 -evp aes-192-cbc
+	openssl speed -engine uadk -evp aes-256-cbc
+	openssl speed -engine uadk -async_jobs 1 -evp aes-256-cbc
+	openssl speed -engine uadk -evp aes-128-ecb
+	openssl speed -engine uadk -async_jobs 1 -evp aes-128-ecb
+	openssl speed -engine uadk -evp aes-192-ecb
+	openssl speed -engine uadk -async_jobs 1 -evp aes-192-ecb
+	openssl speed -engine uadk -evp aes-256-ecb
+	openssl speed -engine uadk -async_jobs 1 -evp aes-256-ecb
+	openssl speed -engine uadk -evp aes-128-xts
+	openssl speed -engine uadk -async_jobs 1 -evp aes-128-xts
+	openssl speed -engine uadk -evp aes-256-xts
+	openssl speed -engine uadk -async_jobs 1 -evp aes-256-xts
+fi
+
+if [[ $algs =~ "SM4" ]]; then
+	echo "testing SM4"
+	openssl speed -engine uadk -evp sm4-cbc
+	openssl speed -engine uadk -async_jobs 1 -evp sm4-cbc
+	openssl speed -engine uadk -evp sm4-ecb
+	openssl speed -engine uadk -async_jobs 1 -evp sm4-ecb
+fi
+
+if [[ $algs =~ "DES" ]]; then
+	echo "testing DES"
+	openssl speed -engine uadk -evp des-ede3-cbc
+	openssl speed -engine uadk -async_jobs 1 -evp des-ede3-cbc
+	openssl speed -engine uadk -evp des-ede3-ecb
+	openssl speed -engine uadk -async_jobs 1 -evp des-ede3-ecb
+fi
 
 #rsa
-openssl speed -elapsed -engine uadk rsa1024
-openssl speed -elapsed -engine uadk -async_jobs 1 rsa1024
-openssl speed -elapsed -engine uadk rsa2048
-openssl speed -elapsed -engine uadk -async_jobs 1 rsa2048
-openssl speed -elapsed -engine uadk rsa4096
-openssl speed -elapsed -engine uadk -async_jobs 1 rsa4096
+if [[ $algs =~ "RSA" ]]; then
+	echo "testing RSA"
+	openssl speed -elapsed -engine uadk rsa1024
+	openssl speed -elapsed -engine uadk -async_jobs 1 rsa1024
+	openssl speed -elapsed -engine uadk rsa2048
+	openssl speed -elapsed -engine uadk -async_jobs 1 rsa2048
+	openssl speed -elapsed -engine uadk rsa4096
+	openssl speed -elapsed -engine uadk -async_jobs 1 rsa4096
+fi
