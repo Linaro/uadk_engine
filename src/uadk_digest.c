ced89dc8f834ba01ba369e6d793634a599565913
Zhiqi Song <songzhiqi1@huawei.com>
2023-12-03 20:52:40 +0800

    uadk_engine: fix env poll timeout in abnormal scenarios
    In abnormal scenarios, if the alg cores of hardware device are
    closed, the hw async task may return busy and can not finish
    the task. There is be a max loop count to avoid infinite loop.
    But there is already a 'MAX_POLL_TIMES' (1000) in uadk default
    poll policy. If engine use 'ENGINE_RECV_MAX_CNT' to control the
    poll loop count, the actual count will be:
    	MAX_POLL_TIMES * ENGINE_RECV_MAX_CNT
    which is too large. This will cause the process use a very long
    time to exit. So modify the env poll loop time count.
    
    Signed-off-by: Zhiqi Song <songzhiqi1@huawei.com>

--- src/uadk_digest.c
+++ src/uadk_digest.c
@@ -391,7 +391,7 @@ static int uadk_e_digest_env_poll(void *ctx)
 		if (ret < 0 || recv == expt)
 			return ret;
 		rx_cnt++;
-	} while (rx_cnt < ENGINE_RECV_MAX_CNT);
+	} while (rx_cnt < ENGINE_ENV_RECV_MAX_CNT);
 
 	fprintf(stderr, "failed to poll msg: timeout!\n");
 
